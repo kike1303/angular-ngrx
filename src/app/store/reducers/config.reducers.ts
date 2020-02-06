4e105d38c73ed800d27c13e47f6346aac5e70d97
Jesus Cuevas <jesuscuevas@iMac-de-Jesus.local>
2020-02-05 23:18:51 -0500

    adding new reducer

---
+++ src/app/store/reducers/config.reducers.ts
@@ -0,0 +1,12 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import * as ConfigActions from '../actions/config.actions';
+import { initialConfigState, IConfigState } from '../state/config.state';
+
+const configReducer = createReducer(
+    initialConfigState,
+    on(ConfigActions.GetConfigSuccess , (state, { payload }) => ({ ...state, payload })),
+)
+
+export function reducer(state: IConfigState | undefined, action: Action) {
+    return configReducer(state, action);
+}
